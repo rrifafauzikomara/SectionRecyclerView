65f95091549fbd4335f72fddeba6edcffd81f03c
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2020-01-20 10:58:23 +0700

    refactoring to android X

--- app/src/main/java/rifafauzi6/id/sectionrecyclerview/MainActivity.java
+++ app/src/main/java/rifafauzi6/id/sectionrecyclerview/MainActivity.java
@@ -1,19 +1,17 @@
 package rifafauzi6.id.sectionrecyclerview;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,7 +25,6 @@ import rifafauzi6.id.sectionrecyclerview.adapter.QuestionAdapter;
 import rifafauzi6.id.sectionrecyclerview.api.BaseApiService;
 import rifafauzi6.id.sectionrecyclerview.api.Server;
 import rifafauzi6.id.sectionrecyclerview.model.Question;
-import rifafauzi6.id.sectionrecyclerview.model.ResponsModelKuesioner;
 import rifafauzi6.id.sectionrecyclerview.model.ResponsModelQuestion;
 
 public class MainActivity extends AppCompatActivity {
